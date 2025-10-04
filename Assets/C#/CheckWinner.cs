3090d09c3e529793f13ecbb31aac0d8f9843027d
Eemeli Väisänen <105064442+iimuli@users.noreply.github.com>
2025-10-04 14:40:56 +0300

    Update CheckWinner.cs
    Some pseudo-ish code written from work

--- Assets/C#/CheckWinner.cs
+++ Assets/C#/CheckWinner.cs
@@ -38,19 +38,19 @@ public static class CheckWinner
         List<Card> cardsToCheck = new List<Card>(player.baseCards);
         cardsToCheck.AddRange(tableCards);
 
-        for(int i = 0;i < player.baseCards.Count;i++)
+        for(int i = 0;i < cardsToCheck.Count;i++)
         {
-            int cardNumber = player.baseCards[i].cardNumber;
+            int cardNumber = cardsToCheck[i].cardNumber;
             if (amountOfSameCards.ContainsKey(cardNumber)) amountOfSameCards[cardNumber]++;
             else amountOfSameCards.Add(cardNumber, 1);
         }
 
         int currentHighestSameCardNumber = 0;
-        int biggestCardNumber = 0;
+        int biggestCardNumber = 0; //DOESNT WORK, RE CHECK ALL CARDS WITH THE NUMBER OF HIGHEST AMOUNT
 
         for(int i = 0; i < amountOfSameCards.Count;i++)
         {
-            if(amountOfSameCards.Count > currentHighestSameCardNumber)
+            if(amountOfSameCards[i] > currentHighestSameCardNumber)
             {
                 currentHighestSameCardNumber = amountOfSameCards[i];
                 biggestCardNumber = amountOfSameCards.Single(s => s.Key == amountOfSameCards[i]).Value;
@@ -87,32 +87,80 @@ public static class CheckWinner
         player.CheckHigherHand(highestHand);
 
         CheckIfBigger(player, highestHand);
-        NumberOfSameSuits(cardsToCheck);
+        NumberOfSameSuits(cardsToCheck, player);
     }
-    static int NumberOfSameSuits(List<Card> cardsToCheck)
+    static void NumberOfSameSuits(List<Card> cardsToCheck, PlayerScript player)
     {
-        int currentAmount = 0;
-        Dictionary<int, int> amountOfSameCards = new Dictionary<int, int>();
+        //WRITTEN ONLINE WITHOUT COMPILER -- pseudo-ish
+        
 
-        return currentAmount;
+        
+        Dictionary<SuitEnum, int> amountOfSameSuits = new Dictionary<SuitEnum, int>();
+        
+        for(int i = 0; i < cardsToCheck.Count; i++)
+        {
+             SuitEnum cardSuit = cardsToCheck[i].cardSuit;
+             if (amountOfSameSuits.ContainsKey(cardSuit)) amountOfSameSuits[cardSuit]++;
+             else amountOfSameSuits.Add(cardSuit, 1);
+        }
+
+        int currentHighestSameSuitNumber = 0;
+        SuitEnum highestAmountSuit;
+
+        for(int i = 0; i < amountOfSameSuits.Count;i++)
+        {
+            if(amountOfSameSuits[i] > currentHighestSameCardNumber)
+            {
+                currentHighestSameCardNumber = amountOfSameSuits[i];
+                highestAmountSuit = amountOfSameSuits[i]; //TRY GET KEY? OR IS THIS OK?
+            }
+        }
+        if(currentHighestSamesuitnumber >= 5)
+        {
+            if(CheckIfBigger(player, HandEnum.Flush))
+            {
+                 int biggestCardNumber = 0;
+                        for(int i = 0; i < cardsToCheck.Count;i++)
+                {
+                if(cardsToCheck[i].cardSuit == highestAmountsuit)
+                {
+                if(cardsToCheck[i].cardNumber > biggestCardNumber) biggestCardNumber = cardsToCheck[i].cardNumber;
+            }
+        
+        }
+        
+            }
+            
+        }
+        
+       
+
+        //Continue checking for other conditions. Or should the code check from highest hand to lowest? or just like this, check same numbers+suits first.
+        //or check pairs+threes+fours in the first "check numbers" code. amount of pairs, amount of threes. cross check for fullhouse, three pairs or double threes.
+        //check straight, add each card into number order and see if 5 straight, if yes, check highst card, also if straight is bigger than 5, check the highest.
+        //END OF ONLINE WRITTEN CODE
+
     }
-    static void CheckIfBigger(PlayerScript player, HandEnum hand)
+    static bool CheckIfBigger(PlayerScript player, HandEnum hand)
     {
         if((int)hand > (int)highestHand)
         {
             highestHand = hand;
             currentWinner = player;
+            return true;
         }
         else if((int)hand == (int)highestHand)
         {
             if(player.highestCard > currentWinner.highestCard)
             {
                 currentWinner = player;
+                return true;
             }
             else if(player.highestCard == currentWinner.highestCard)
             {
                 currentWinner = null;
             }
         }
+        return false;
     }
 }
